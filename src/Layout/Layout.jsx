970c6ef213bc4570f5803b2d8057eea7eede601e
unknown <shaimazerin46@gmail.com>
2025-01-04 22:45:13 +0600

    main layout created

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+
+
+const Layout = () => {
+    return (
+        <div>
+            <h3>This is main Layout</h3>
+        </div>
+    );
+};
+
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+
+import * as React from "react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout/Layout.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+  },
+]);
+
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
