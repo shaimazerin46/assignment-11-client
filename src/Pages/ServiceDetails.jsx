f037862b561542fb73ff391b35d7650d13ea42ec
unknown <shaimazerin46@gmail.com>
2025-01-07 00:24:46 +0600

    add review functionality done

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -1,12 +1,17 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Rating from "react-rating";
 import { useParams } from "react-router-dom";
+import AuthContext from "../Context/AuthContext/AuthContext";
+import Swal from "sweetalert2";
 
 
 const ServiceDetails = () => {
     const {id} = useParams();
-    const [data,setData] = useState(null)
+    const [data,setData] = useState(null);
+    const [rating,setRating] = useState(0);
+    const textRef = useRef();
+    const {user} = useContext(AuthContext);
     
     useEffect(()=>{
         axios.get(`http://localhost:5000/services/${id}`)
@@ -15,6 +20,44 @@ const ServiceDetails = () => {
         setData(data)
     })
     },[])
+
+    const handleReview = (e)=>{
+       
+        e.preventDefault();
+        const text = textRef.current.value;
+        const userInfo = {
+            email: user.email,
+            name: user.displayName,
+            photo: user.photoURL
+        }
+        const postedDate = new Date().toISOString().split('T')[0];
+        console.log(rating,text,userInfo,postedDate)
+        const review = {
+            userInfo,
+            text,
+            rating,
+            postedDate
+
+        }
+        axios.post('http://localhost:5000/reviews',review)
+        .then(res=>{
+            console.log(res.data)
+            if(res.data.insertedId){
+                Swal.fire({
+                    title: "Good job!",
+                    text: "Thank you for your review",
+                    icon: "success"
+                  });
+            }
+        })
+        .catch(err=>{
+            Swal.fire({
+                icon: "error",
+                text: (err.message),
+              });
+        })
+
+    }
     return (
         <div>
             <h3 className="text-3xl text-center font-bold py-20">Service details</h3>
@@ -36,17 +79,22 @@ const ServiceDetails = () => {
             </div>
             <div>
                 <h3 className="text-xl font-bold mb-20 text-center">Add review</h3>
-                <div className="flex flex-col space-y-5">
+                <div >
                     
-                    <textarea
-                        placeholder="write your review"
-                        className="textarea textarea-bordered textarea-sm w-full max-w-xs"></textarea>
-
-                        <Rating
-                        emptySymbol="far fa-star"
-                        fullSymbol="fas fa-star"
-                        ></Rating>
-                        <button className="btn btn-primary w-40">Add review</button>
+                   <form onSubmit={handleReview} className="flex flex-col space-y-5">
+                            <textarea
+                            ref={textRef}
+                                placeholder="write your review"
+                                className="textarea textarea-bordered textarea-sm w-full max-w-xs"></textarea>
+                            <span>Ratings:</span>
+                            <Rating
+                                emptySymbol="far fa-star"
+                                fullSymbol="fas fa-star"
+                                initialRating={rating}
+                                onChange={(newRating)=>setRating(newRating)}
+                            ></Rating>
+                            <button className="btn btn-primary w-40">Add review</button>
+                   </form>
                 </div>
             </div>
             </div>
